942a2c5eda210df5d6fea807cc7e690e5700865a
Armani Simmons <fibonaccy@protonmail.com>
2023-10-02 05:41:27 -0400

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+const employee = {
+    name: "John Doe",
+    streetAddress: "123 Main Street",
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  module.exports = {
+    updateEmployeeWithKeyAndValue,
+    destructivelyUpdateEmployeeWithKeyAndValue,
+    deleteFromEmployeeByKey,
+    destructivelyDeleteFromEmployeeByKey,
+  };
+  
+  console.log(updateEmployeeWithKeyAndValue(employee, "name", "Jane Doe"));
+  console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "456 Elm Street"));
+  console.log(deleteFromEmployeeByKey(employee, "name"));
+  console.log(destructivelyDeleteFromEmployeeByKey(employee, "streetAddress"));
